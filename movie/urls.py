0c912fb950ef759a182966e346b9a228316b89d5
DOR <dbtzur@gmail.com>
2022-07-05 15:17:04 +0300

    commet files

--- movie/urls.py
+++ movie/urls.py
@@ -4,12 +4,12 @@ app_name = 'movie'
 
 urlpatterns = [
 
-    path('list_of_movies', MovieList.as_view(), name='movie_list'),
-    path('genres/<str:category>', MovieCategory.as_view(), name='movie_category'),
-    path('language/<str:lang>', MovieLanguage.as_view(), name='movie_language'),
-    # path('search/', MovieSearch.as_view(), name='movie_search'),
-    path('<slug:slug>', MovieDetail.as_view(), name='movie_detail'),
-    path('year/<int:year>', MovieYear.as_view(), name='movie_year'),
+    # path('list_of_movies', MovieList.as_view(), name='movie_list'),
+    # path('genres/<str:category>', MovieCategory.as_view(), name='movie_category'),
+    # path('language/<str:lang>', MovieLanguage.as_view(), name='movie_language'),
+    # # path('search/', MovieSearch.as_view(), name='movie_search'),
+    # path('<slug:slug>', MovieDetail.as_view(), name='movie_detail'),
+    # path('year/<int:year>', MovieYear.as_view(), name='movie_year'),
 
 ]
 
